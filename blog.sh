eba32c3d4cd9e1bf4ba3d9e7312997901de21fdf
KevinXuxuxu <kevin.xu.fangzhou@gmail.com>
2021-12-03 10:25:07 -0800

    Switch to port 5001 for local server to avoid mac rapport

--- blog.sh
+++ blog.sh
@@ -1,7 +1,7 @@
 while getopts sgpd flag
 do
     case "$flag" in
-        s) flask run -h 0.0.0.0;;
+        s) flask run -h 0.0.0.0 -p 5001;;
         g) python3 generate.py;;
         p) pushd build && git add . && git commit -am "update" && git push && popd;;
         d) pushd build && git status && git diff && popd;;
