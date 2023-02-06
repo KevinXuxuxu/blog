827122fb5d2acb25426a6707fac4469daf6a01fa
KevinXuxuxu <kevin.xu.fangzhou@gmail.com>
2023-02-06 00:10:40 -0800

    move blog generating destination one level down

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ from utils import *
 
 app = Flask(__name__)
 
-app.config['FREEZER_DESTINATION_IGNORE'] = ['.git*']
+app.config['FREEZER_DESTINATION_IGNORE'] = ['.git*', 'online_tool']
 
 
 @app.route("/")
@@ -17,19 +17,19 @@ def index():
     tags = get_all_tags(all_posts)
     return render_template('index.html', rendered_content=html, posts=all_posts, tags=tags)
 
-@app.route("/category/<category>/")
+@app.route("/blog/category/<category>/")
 def category(category):
     all_posts = get_all_posts_with_metadata()
     filtered_posts = [p for p in all_posts if p.category == category]
     return render_template('category.html', category=category, posts=filtered_posts)
 
-@app.route("/tag/<tag>/")
+@app.route("/blog/tag/<tag>/")
 def tag(tag):
     all_posts = get_all_posts_with_metadata()
     filtered_posts = [p for p in all_posts if tag in p.tags]
     return render_template('tag.html', tag=tag, posts=filtered_posts)
 
-@app.route("/post/<path_title>/")
+@app.route("/blog/post/<path_title>/")
 def post(path_title):
     md = get_local_content('posts', path_title)
     parsed_post = parse_post_metadata(path_title, md)
--- blog.sh
+++ blog.sh
@@ -1,7 +1,7 @@
 while getopts sgpd flag
 do
     case "$flag" in
-        s) docker run --rm -p 5001:5001 -v `pwd`:/blog -w /blog -it fzxu/blog flask run -h 0.0.0.0 -p 5001;;
+        s) docker run --rm -p 8000:8000 -v `pwd`/build:/build -w /build -it fzxu/blog python3 -m http.server;;
         g) docker run --rm -v `pwd`:/blog -w /blog -it fzxu/blog python3 generate.py;;
         p) pushd build && git add . && git commit -am "update" && git push && popd;;
         d) pushd build && git status && git diff && popd;;
