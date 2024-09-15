9b9a9e11c950abe2b175407d3d991b82c1f5f46b
KevinXuxuxu <kevin.xu.fangzhou@gmail.com>
2024-09-15 12:34:07 -0700

    dark-mode: detect system color mode

--- static/script/color_theme.js
+++ static/script/color_theme.js
@@ -30,13 +30,24 @@ function set_color_theme(color_theme) {
     }
 }
 
+function get_initial_color_theme() {
+    var color_theme = getCookie('color_theme');
+    if (color_theme === undefined) {
+        color_theme = 'light';
+        if (window.matchMedia('(prefers-color-scheme: dark)').matches) {
+            color_theme = 'dark';
+        }
+    }
+    return color_theme;
+}
+
 function init_color_theme() {
-    var color_theme = getCookie('color_theme', 'light');
+    var color_theme = get_initial_color_theme();
     document.documentElement.setAttribute('data-theme', color_theme);
 }
 
 function init_color_switch() {
-    var color_theme = getCookie('color_theme', 'light');
+    var color_theme = get_initial_color_theme();
     var moon = document.getElementById('moon');
     var checkbox = document.getElementById('theme-checkbox');
     if (color_theme === 'dark') {
@@ -51,16 +62,22 @@ function init_color_switch() {
 
 function switch_color_theme() {
     let color_theme = getCookie('color_theme', 'light');
-    if (color_theme === 'dark')  {
+    if (color_theme === 'dark') {
         set_color_theme('light');
     } else {
         set_color_theme('dark');
     }
 }
 
-document.addEventListener("DOMContentLoaded", function() {
+document.addEventListener("DOMContentLoaded", function () {
     const element = document.getElementById("color-switch");
     if (element) {
         init_color_switch();
     }
 });
+
+window.matchMedia("(prefers-color-scheme: dark)")
+    .addEventListener("change", (event) => {
+        const theme = event.matches ? "dark" : "light";
+        set_color_theme(theme);
+    });
