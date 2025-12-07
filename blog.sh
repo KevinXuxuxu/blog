e382a780c12e8bb3706e9fc3c5f65b7ad51a70cf
KevinXuxuxu <kevin.xu.fangzhou@gmail.com>
2025-12-06 23:08:44 -0800

    [utils] specify port for ./blog.sh -s and -v

--- blog.sh
+++ blog.sh
@@ -1,8 +1,8 @@
 while getopts sgpdnv flag
 do
     case "$flag" in
-        v) pushd build && uv run python3 -m http.server && popd;;
-        s) OPENCODER_URL_PREFIX='/opencoder' uv run flask run -h 0.0.0.0 -p 8000;;
+        v) pushd build && uv run python3 -m http.server $2 && popd;;
+        s) OPENCODER_URL_PREFIX='/opencoder' uv run flask run -h 0.0.0.0 -p $2;;
         g) OPENCODER_URL_PREFIX='/opencoder' uv run python3 generate.py;;
         p) pushd build && git add . && git commit -am "update" && git push && popd;;
         d) pushd build && git status && git diff && popd;;
